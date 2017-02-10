7cea970801bb89f79f94d8a1bb84512a75efe86a
Kieran Browne <kierantbrowne@gmail.com>
2017-02-10 18:27:23 +1100

    initial :beers:

---
+++ project.clj
@@ -0,0 +1,52 @@
+(defproject pint "0.1.0-SNAPSHOT"
+  :dependencies [[org.clojure/clojure "1.8.0"]
+                 [org.clojure/clojurescript "1.9.229"]
+                 [reagent "0.6.0"]
+                 [secretary "1.2.3"]]
+
+  :min-lein-version "2.5.3"
+
+  :source-paths ["src/clj"]
+
+  :plugins [[lein-cljsbuild "1.1.4"]]
+
+  :clean-targets ^{:protect false} ["resources/public/js/compiled"
+                                    "target"]
+
+  :figwheel {:css-dirs ["resources/public/css"]}
+
+
+  :repl-options {:nrepl-middleware [cemerick.piggieback/wrap-cljs-repl]}
+
+  :profiles
+  {:dev
+   {:dependencies [
+                   [figwheel-sidecar "0.5.8"]
+                   [com.cemerick/piggieback "0.2.1"]]
+
+    :plugins      [[lein-figwheel "0.5.8"]
+                   [cider/cider-nrepl "0.13.0"]]
+    }}
+
+  :cljsbuild
+  {:builds
+   [{:id           "dev"
+     :source-paths ["src/cljs"]
+     :figwheel     {:on-jsload "pint.core/reload"}
+     :compiler     {:main                 pint.core
+                    :optimizations        :none
+                    :output-to            "resources/public/js/compiled/app.js"
+                    :output-dir           "resources/public/js/compiled/dev"
+                    :asset-path           "js/compiled/dev"
+                    :source-map-timestamp true}}
+
+    {:id           "min"
+     :source-paths ["src/cljs"]
+     :compiler     {:main            pint.core
+                    :optimizations   :advanced
+                    :output-to       "resources/public/js/compiled/app.js"
+                    :output-dir      "resources/public/js/compiled/min"
+                    :closure-defines {goog.DEBUG false}
+                    :pretty-print    false}}
+
+    ]})
---
+++ src/cljs/pint/core.cljs
@@ -0,0 +1,81 @@
+(ns pint.core
+  (:require-macros [secretary.core :refer [defroute]])
+  (:import goog.History)
+  (:require
+   [secretary.core :as secretary]
+   [goog.events :as events]
+   [goog.history.EventType :as EventType]
+   [reagent.core :as r :refer [atom cursor]]
+   ))
+
+
+
+(defonce debug?
+  ^boolean js/goog.DEBUG)
+
+(defonce state
+  (atom
+   {:page :nil}))
+
+
+(defn hook-browser-navigation! []
+  (doto (History.)
+    (events/listen
+     EventType/NAVIGATE
+     (fn [event]
+       (secretary/dispatch! (.-token event))))
+    (.setEnabled true)))
+
+(defn app-routes []
+  ;; (secretary/set-config! :prefix "#")
+
+  (defroute "/" []
+    (swap! state assoc :page :home))
+
+  (defroute "/component/:type/:key" [type key]
+    (do
+      (swap! state assoc :page :component)
+      (swap! state assoc :component {:type type :key key})
+      ))
+
+  (hook-browser-navigation!))
+
+
+
+;; Pages
+
+(defn home [ratom]
+  [:div [:h1 "Home Page"]
+   [:p "FIXME"]
+   [:a {:href "/about"} "about page"]])
+
+(defn component-page [ratom]
+  [:div [:h1 "Component Page"]
+   [:a {:href "/"} (str (:component @state))]])
+
+
+;; Initialize App
+
+(defmulti page identity)
+(defmethod page :home [] home)
+(defmethod page :component [] component-page)
+(defmethod page :default [] (fn [_] [:div]))
+
+(defn current-page [ratom]
+  (let [page-key (:page @ratom)]
+    [(page page-key) ratom]))
+
+(defn dev-setup []
+  (when debug?
+    (enable-console-print!)
+    (println "dev mode")
+    ))
+
+(defn reload []
+  (r/render [current-page state]
+                  (.getElementById js/document "app")))
+
+(defn ^:export main []
+  (dev-setup)
+  (app-routes)
+  (reload))
