5e4f145f72413c1dd9d0a4d3da842ec9754e0d9f
Kieran Browne <kierantbrowne@gmail.com>
2017-03-13 15:54:08 +1100

    functioning auto microclass generating function (pintificate)

--- src/clj/pint/utils.clj
+++ src/clj/pint/utils.clj
@@ -1,70 +1,46 @@
 (ns pint.utils
   (:require [garden.core :as garden :refer [css]]
-            [clojure.math.combinatorics :as combo]
+            [clojure.math.combinatorics :refer [cartesian-product]]
             [clojure.string :as str]))
 
 (defn unit [unit value]
   (str value unit))
 
-(defn comb [props values]
-  (map (fn [x] {:prop (first x) :value(last x)}) 
-    (combo/cartesian-product props values)))
+(defn comb
+  "Returns all combinations of properties and values"
+  [props values]
+  (map (partial apply hash-map)
+    (cartesian-product props values)))
 
-(defn cssify 
-  [combos]
-  (map (fn [combo]
-         (css
-           [(str "."
-                 (-> combo :prop first)
-                 (-> combo :value first))
+(def shorten
+  "Auto shortener for properties and values"
+  (comp
+   (partial apply str)
+   (partial map #(if (re-find #"\d" %) % ((comp str first) %)))
+   (partial re-seq #"\w+|\d(?!\S)|%")
+   str))
 
-            { (-> combo :prop last)
-              (-> combo :value last) }
+(def pint-class
+  "Convert cssmap into pint microclass"
+  (comp keyword (partial str ".") shorten))
 
-            ])
-         ; (css [(str (:short (:prop combo)))])
-         )
-    combos))
-
-(def zip (partial map vector))
-
-; auto shortener
-(defn shorten [property]
-  (str/join (map first
-    (filter 
-      (complement empty?) 
-      (str/split (str property) #":|-"))))
-  )
-
-(def splitter (partial re-seq #"\w+|\d(?!\S)|%"))
-
-(splitter "display-inline:block1,10:")
-
-(defn shorten-value [value]
-  (str/join
-   (map first
-        (filter 
-         (complement empty?) 
-         (str/split (str property) #":|-"))))
-  )
-
-(map splitter 
-     ["100px" "10%" "block" "inline-block" "left" "100vh" "none"]
-     )
-
-(defn auto-val-shortener [property]
-  (str/join (map first
-    (filter 
-      (complement empty?) 
-      (str/split (str "20rem") #":|-"))))
-  )
-
+(defn pintificate
+  "Generate microclass for every combination of props and values."
+  [props values]
+  (apply merge
+         (map
+          (fn [x] {(pint-class x) x})
+          (comb props values))))
 
 (def pints
-  {:.db {:display "block"}
-   :.dib {:display "inline-block"}
-   :.fw100 {:font-weight "100"}
-   })
+  "Collate all pints"
+  (merge
+   (pintificate [:display] [:inline-block :block :flex])
+   (pintificate [:font-weight] (range 100 1000 100))
+   (pintificate [:font-style] [:italic])
+   (pintificate [:font-size] ["10px"])
+   ))
+
 
 (spit "resources/public/css/pint-12-30px.css"
       (apply str (map css (vec pints))))
@@ -77,20 +53,17 @@
   )
 
 (def classes
-  "get classes from hiccup keyword"
+  "Get classes from hiccup keyword"
   (comp
    (partial map keyword)
    (partial re-seq #"\.[^\.]+")
    str))
 
-(map classes post-tile)
 
 (defn collate-styles [hiccup-key]
   (apply merge
          (map (partial get pints)
               (classes hiccup-key))))
-
-(gensym "pint")
 
 (defn traditional-css
   "Generate traditional nested css structure from markup."
@@ -98,8 +71,7 @@
   (into
    [(first markup)
     (collate-styles (first markup))]
-   (map traditional-css (filter vector? (rest markup)))
-   ))
+   (map traditional-css (filter vector? (rest markup)))))
 
 (css (traditional-css post-tile))
 (traditional-css post-tile)
